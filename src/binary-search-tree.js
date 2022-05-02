c4d105f34f6d5818a59bc72a5b4e1eb1a90cdbd4
Oksana Astapova <astapova.frontend@gmail.com>
2022-05-02 17:23:15 +0300

    half binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,17 +6,56 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
 class BinarySearchTree {
+  constructor() {
+    this.ROOT = null; // root of a binary search tree
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.ROOT;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let newNode = new Node(data);
+                  
+    if(this.ROOT === null)
+        this.ROOT = newNode;
+    else
+        // find the correct position in the tree and add the node
+        this.insertNode(this.ROOT, newNode);
   }
+  // Method to insert a node in a tree; it moves over the tree to find the location to insert a node with a given data
+  insertNode(node, newNode){
+    // if the data is less than the node data move left of the tree
+    if(newNode.data < node.data)
+    {
+        // if left is null insert node here
+        if(node.left === null)
+            node.left = newNode;
+        else
+             // if left is not null recur until null is found
+            this.insertNode(node.left, newNode);
+    }
+ 
+    // if the data is more than the node data move right of the tree
+    else
+    {
+        // if right is null insert node here
+        if(node.right === null)
+            node.right = newNode;
+        else
+             // if right is not null recur until null is found
+            this.insertNode(node.right,newNode);
+    }
+}
 
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
@@ -28,11 +67,62 @@ class BinarySearchTree {
     // remove line with error and write your code here
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  remove(data) {
+    this.ROOT = this.removeNode(this.ROOT, data);
 
+  }
+  removeNode(node, key)
+  {     
+    // if the root is null then tree is empty
+    if(node === null)
+        return null;
+ 
+    // if data to be delete is less than roots data then move to left subtree
+    else if(key < node.data)
+    {
+        node.left = this.removeNode(node.left, key);
+        return node;
+    }
+ 
+    // if data to be delete is greater than roots data then move to right subtree
+    else if(key > node.data)
+    {
+        node.right = this.removeNode(node.right, key);
+        return node;
+    }
+ 
+    // if data is similar to the root's data then delete this node
+    else
+    {
+         // deleting node with no children
+        if(node.left === null && node.right === null)
+        {
+            node = null;
+            return node;
+        }
+ 
+        // deleting node with one children
+        if(node.left === null)
+        {
+            node = node.right;
+            return node;
+        }
+         
+        else if(node.right === null)
+        {
+            node = node.left;
+            return node;
+        }
+ 
+        // Deleting node with two children minimum node of the right subtree is stored in aux
+        let aux = this.min(node.right);
+        node.data = aux.data;
+ 
+        node.right = this.removeNode(node.right, aux.data);
+        return node;
+    }
+   }
+   
   min() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
